1016d33a6680d49e5d12348fe237d223ca83030f
Subhajit Rajak <subhajitrajak2001@gmail.com>
2024-07-16 01:10:28 +0530

    Fixed issue - minor ide warnings

--- app/src/main/java/com/subhajitrajak/notesapp/NotesAdapter.kt
+++ app/src/main/java/com/subhajitrajak/notesapp/NotesAdapter.kt
@@ -10,7 +10,6 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.navigation.Navigation
 import androidx.recyclerview.widget.RecyclerView
-import com.google.firebase.auth.FirebaseAuth
 
 class NotesAdapter(private var notes: List<Note>, private val currentUser: String, context: Context) : RecyclerView.Adapter<NotesAdapter.NotesViewHolder>()     {
     private val db: NotesDatabaseHelper = NotesDatabaseHelper(context)
